cd824104c6003db8c3683cf41a79b8f8e735f1ab
Eduarda Calixto <100138993+eduardaclx@users.noreply.github.com>
2022-11-29 22:41:16 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -16,8 +16,7 @@ idDado INT AUTO_INCREMENT PRIMARY KEY,
 memoriaDisponivel DECIMAL,
 usoProcessador DECIMAL,
 columnData TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-fkHardware VARCHAR(20),
-FOREIGN KEY (fkHardware) REFERENCES Hardware(idHardware)
+fkDesktop CHAR(8) UNIQUE
 );
 
 CREATE TABLE Disco (
@@ -32,6 +31,5 @@ tamanho INT,
 tamanhoAtualFila INT,
 tempoTransferencia INT,
 columnData TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-fkHardware VARCHAR(20),
-FOREIGN KEY (fkHardware) REFERENCES Hardware(idHardware)
+fkDesktop CHAR(8) UNIQUE
 );
